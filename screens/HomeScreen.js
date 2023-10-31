61bdcbacc901e54dd398933a9c409bb74946e4e4
LaxmanZ <bhajantrilaxman2001@gmail.com>
2023-10-31 10:49:22 +0530

    integrated with mongoDB

---
+++ screens/HomeScreen.js
@@ -0,0 +1,16 @@
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const HomeScreen = () => {
+  return (
+    <ScrollView style={{ marginTop: 0 }}>
+      <View>
+        <Text>HomeScreen</Text>
+      </View>
+    </ScrollView>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
